5034d35ce50e1914b5efad7d8aa31132d148c252
stone205446 <stone205446@AISD92106.aisd.net>
2011-11-30 08:46:40 -0600

    Major changes to the physics of the game. I added friction so the car would slow down if not touched. More realistic features need to be added though. I got my car to move and rotate correctly. I will be adding a track to race on as well as environmental obstacles such as grass and sand pits.

--- src/net/mrfornal/lts/LogansDemo.java
+++ src/net/mrfornal/lts/LogansDemo.java
@@ -4,12 +4,17 @@
  */
 package net.mrfornal.lts;
 
+import net.mrfornal.game.*;
+import org.newdawn.slick.Color;
+import net.mrfornal.entity.BasicTestEntity;
+import net.mrfornal.entity.Entity;
 import net.mrfornal.entity.EntityManager;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
 
 /**
  *
@@ -29,13 +34,22 @@ public class LogansDemo extends BasicGame
     public void init(GameContainer container) throws SlickException
     {
         
+        eManager.addEntity(new Car(new Vector2f(), 0));
+
+        for (Entity e : eManager.getAllEntities())
+        {
+            e.init(container);
+        }        
     }
     
     @Override
     public void update(GameContainer container, int delta) throws SlickException
     {
         Input i = container.getInput();
-        
+        for (Entity e : eManager.getAllEntities())
+        {
+            e.update(container, delta);
+        }
         if(i.isKeyPressed(Input.KEY_ESCAPE))
         {
             System.exit(0);
@@ -46,7 +60,10 @@ public class LogansDemo extends BasicGame
     @Override
     public void render(GameContainer container, Graphics g) throws SlickException
     {
-        
+        for (Entity e : eManager.getAllEntities())
+        {
+            e.render(container, g);
+        }
     }
     
 }
